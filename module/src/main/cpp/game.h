7a598bc05d248f9c657c2aca0f11d2b6da75aca3
ThewsyRum <102466085+ThewsyRum@users.noreply.github.com>
2024-01-15 18:35:45 -0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.mayoninjagames.zombiecubes2"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
